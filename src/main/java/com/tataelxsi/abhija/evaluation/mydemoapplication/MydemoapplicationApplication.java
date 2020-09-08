422ae76b86aa184ca580859722e97c15405e0645
ajayan508@cable.comcast.com <ajayan508@cable.comcast.com>
2020-09-08 17:54:42 +0530

    Test App

---
+++ src/main/java/com/tataelxsi/abhija/evaluation/mydemoapplication/MydemoapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.tataelxsi.abhija.evaluation.mydemoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydemoapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydemoapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tataelxsi/abhija/evaluation/mydemoapplication/MydemoapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tataelxsi.abhija.evaluation.mydemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydemoapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
